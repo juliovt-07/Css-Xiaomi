5b59223c20e6b14af7ff22d19bdbbc9c2a17027e
juliovt-07 <jcproducoes.verytop@gmail.com>
2020-05-25 03:07:32 -0300

    resolvendo bug da hora

--- Js/time.js
+++ Js/time.js
@@ -1,6 +1,6 @@
 function hora(){
     var now = new Date();
-    now = ("00"+now.getHours()).slice(-2)+':'+now.getMinutes();
+    now = ("00"+now.getHours()).slice(-2)+':'+("00"+now.getMinutes()).slice(-2);
     window.document.getElementById('time').innerHTML = now;
 }
 window.onload = setInterval(hora,1000);
